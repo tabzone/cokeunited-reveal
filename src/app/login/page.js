ca49f303cbcfe7960a57dbad9a05304353b581c6
Nobin Babu <105436510+nobinbabu94@users.noreply.github.com>
2026-06-15 18:34:01 +0530

    Delete src directory
    removed components outside app

--- src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-// app/login/page.js
-
-export default function LoginPage() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      Login Form Here
-    </div>
-  );
-}
