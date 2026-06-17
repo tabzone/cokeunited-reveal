2c1885877a9ee7e95ecdc0015abd473be64e35c5
nobinbabu94 <nobins73@gmail.com>
2026-06-17 22:30:06 +0530

    layout changed and sidebar removed

--- app/components/layout/AppLayout.jsx
+++ app/components/layout/AppLayout.jsx
@@ -1,15 +1,12 @@
-"use client";
 
 import { useState } from "react";
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 
 export default function AppLayout({ children }) {
-  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="flex h-screen">
-      <Sidebar isOpen={isOpen} onToggleSidebar={() => setIsOpen(!isOpen)} />
+      {/* <Sidebar isOpen={isOpen} onToggleSidebar={() => setIsOpen(!isOpen)} /> */}
       <div className="flex-1 flex flex-col overflow-hidden">
         <Navbar />
 
--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect, useRef } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { useAuth } from "../AuthProvider";
+import Link from "next/link";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -91,9 +92,19 @@ export default function Navbar() {
           </div>
         </div>
       )}
-
+        <div className="flex items-center">
+      <Link href="/" className="flex items-center pl-2 pr-24">
+        <img
+          src="/Parkers-Kitchen-Logo.png"
+          alt="Parker's Kitchen"
+          className="h-16   w-auto"
+        />
+      </Link>
+      </div>
       {/* Report Details */}
       <div className="flex-1 flex justify-start">
+
+
         <div className="flex items-center gap-4 text-sm text-gray-600">
           <span className="font-medium text-gray-800">
             Category Report
