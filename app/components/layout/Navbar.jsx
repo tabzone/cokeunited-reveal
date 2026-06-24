b2f309e9003b5fbb75145a8c1fb09ffc4e736783
Tabzone <tabrez@gmail.com>
2026-06-24 09:36:56 -0700

    Update date information in Navbar component

--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -147,7 +147,7 @@ export default function Navbar() {
           <span>
             Data Last Updated:
             <span className="ml-1 font-medium text-gray-800 dark:text-white">
-              6/12/2026
+              6/24/2026
             </span>
           </span>
 
@@ -156,7 +156,7 @@ export default function Navbar() {
           <span>
             WE:
             <span className="ml-1 font-medium text-gray-800 dark:text-white">
-              6/12 (Week 24)
+              6/19/2026 (Week 25)
             </span>
           </span>
         </div>
